b0ddf5ad0e3c337d1e596d41c30d4ae35afca9bb
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2019-01-24 20:11:38 +0000

    Create safar.js

---
+++ safar.js
@@ -0,0 +1,8 @@
+$(document).ready(function()
+{
+   if($("#blogID").attr("blogID")!="9047105399567810678" )
+   if($("#blogID").attr("blogID")!="5630986621325805078" )
+ {
+   window.location.href="https://www.almosamm.com/";
+ }
+    });
