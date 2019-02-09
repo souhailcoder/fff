a38703496d9912a5b2a1c3e9b8842a032b37c664
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2019-02-09 12:59:23 +0000

    Create mcode.js

---
+++ mcode.js
@@ -0,0 +1,9 @@
+$(document).ready(function()
+{
+   if($("#blogID").attr("blogID")!="3254224716543245501" )
+   if($("#blogID").attr("blogID")!="5082737812476014126" )
+
+ {
+   window.location.href="https://www.almosamm.com/";
+ }
+    });
